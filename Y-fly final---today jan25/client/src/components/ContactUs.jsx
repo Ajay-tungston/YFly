2c87a24f9b84439bfa524c6dc21e8b765d7c3af9
JesnaJamal312 <jesna@tungstonlabs.com>
2025-04-09 17:41:55 +0530

    contact

--- Y-fly final---today jan25/client/src/components/ContactUs.jsx
+++ Y-fly final---today jan25/client/src/components/ContactUs.jsx
@@ -22,20 +22,20 @@ export default function ContactForm() {
 
       <div className="flex-grow flex items-center justify-center p-6 bg-white mt-12 rounded-t-[72px] rounded-b-[72px]">
         <div className="w-full max-w-6xl p-16 md:p-10 rounded-2xl bg-white ">
-          <h2 className="text-2xl md:text-3xl font-lato text-center">Get in Touch with Us!</h2>
-          <p className="text-center text-gray-600 mb-6">Need assistance? Reach out to us via email, and we’ll respond as soon as possible.</p>
-          <h2 className="text-2xl md:text-3xl font-lato text-center">Contact us!</h2>
+          <h2 className="text-[34px] md:text-3xl font-lato text-center mb-8">Get in Touch with Us!</h2>
+          <p className="text-center text-gray-600 mb-6 font-lato">Need assistance? Reach out to us via email, and we’ll respond as soon as possible.</p>
+          <h2 className="text-2xl md:text-3xl font-lato text-center ">Contact us!</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="w-full">
+            <div className="w-full  ">
               <iframe
-                className="w-full h-72 md:h-80 rounded-lg"
+                className="w-full h-82 md:h-[500px] rounded-lg mt-10"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.476456861654!2d76.579452!3d9.9782539!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080d3e5e92c7c3%3A0x3f3c3a8b8e8f3c77!2sYFly%20International!5e0!3m2!1sen!2sin!4v1614578568576!5m2!1sen!2sin"
                 allowFullScreen=""
                 loading="lazy"
               ></iframe>
             </div>
 
-            <div>
+            <div className=" mt-10">
               <div className="text-gray-700 mb-4 font-lato">
                 <p><strong>Email:</strong> info@yflyinternational.com</p>
                 <p><strong>Phone:</strong> +91 9947 788222</p>
